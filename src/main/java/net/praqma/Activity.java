49a1c61ed3887fc6cabbc551b589477908c9ae44
wolfgarnet <wolfgarnet@gmail.com>
2010-10-15 15:45:38 +0200

    Added Activity class

---
+++ src/main/java/net/praqma/Activity.java
@@ -0,0 +1,38 @@
+package net.praqma;
+
+class Activity extends ClearBase
+{
+	private String fqactivity = null;
+	private String shortname  = null;
+	private String changeset  = null;
+	private String stream     = null;
+	private String pvob       = null;
+	
+	public Activity( String fqactivity, boolean trusted )
+	{
+		logger.trace_function();
+		
+		String[] res = TestComponent( fqactivity );
+		if( res == null )
+		{
+			logger.log( "ERROR: Activity constructor: The first parameter ("+fqactivity+") must be a fully qualified activity in the format: activity\\@\\PVOB" + linesep, "error" );
+			System.err.println( "ERROR: Activity constructor: The first parameter ("+fqactivity+") must be a fully qualified activity in the format: activity\\@\\PVOB" + linesep );
+			System.exit( 1 );
+		}
+		
+		this.fqactivity = fqactivity;
+		this.fqname     = fqactivity;
+		this.shortname  = res[0];
+		this.pvob       = res[1];
+		
+		if( !trusted )
+		{
+			// cleartool desc activity:$fqactivity 
+			String cmd = "desc activity:"+fqactivity;
+			Cleartool.run( cmd );
+			/* CHW: Catch any output to stderr, if so, fail! */
+			
+		}
+	}
+	
+}
