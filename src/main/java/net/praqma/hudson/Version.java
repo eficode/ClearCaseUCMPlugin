9362d7c13f3319b21fa5b61d44034a1759f4f087
wolfgarnet <wolfgarnet@gmail.com>
2011-03-01 14:31:02 +0100

    More pom.xml

--- src/main/java/net/praqma/hudson/Version.java
+++ src/main/java/net/praqma/hudson/Version.java
@@ -6,10 +6,10 @@ package net.praqma.hudson;
  */
 public class Version
 {
-	private static final String major    = "xx"; // buildnumber.major
-	private static final String minor    = "xx"; // buildnumber.minor
-	private static final String patch    = "xx"; // buildnumber.patch
-	private static final String sequence = "xx"; // buildnumber.sequence
+	private static final String major    = "0"; // buildnumber.major
+	private static final String minor    = "1"; // buildnumber.minor
+	private static final String patch    = "5"; // buildnumber.patch
+	private static final String sequence = "hej"; // buildnumber.sequence
 	
 	public static final  String version  = major + '.' + minor + '.' + patch + '.' + sequence;
 }
