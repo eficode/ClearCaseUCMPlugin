ef2abfc2e4be0b05f01ec0519bcd92f2547b4a44
wolfgarnet <wolfgarnet@gmail.com>
2011-03-04 10:25:25 +0100

    Fixed pucm_baseline issue

--- src/main/java/net/praqma/hudson/scm/PucmScm.java
+++ src/main/java/net/praqma/hudson/scm/PucmScm.java
@@ -174,6 +174,7 @@ public class PucmScm extends SCM
 			{
 				state.setBaseline( UCMEntity.GetBaseline( baselinename ) );
 				integrationstream = bl.GetStream();
+				state.setStream( state.getBaseline().GetStream() );
 				consoleOutput.println( "[PUCM] Starting parameterized build with a pucm_baseline.\nUsing baseline: " + baselinename + " from integrationstream " + integrationstream.GetShortname() );
 			}
 			catch ( UCMException e )
