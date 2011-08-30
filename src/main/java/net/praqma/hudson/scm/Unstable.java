b61cd9b2de02ce325ce2e8dc751da899f25da28b
wolfgarnet <wolfgarnet@gmail.com>
2011-08-30 15:07:02 +0200

    Added Unstable class

---
+++ src/main/java/net/praqma/hudson/scm/Unstable.java
@@ -0,0 +1,43 @@
+package net.praqma.hudson.scm;
+
+import java.io.Serializable;
+
+public class Unstable implements Serializable {
+
+	private static final long serialVersionUID = -133143812099625188L;
+
+	public enum Type {
+		successful,
+		failed
+	}
+	
+	private Type type;
+	
+	public Unstable( Type type ) {
+		this.type = type;
+	}
+	
+	public Unstable( String type ) {
+		if( type.equalsIgnoreCase( "successful" ) ) {
+			this.type = Type.successful;
+		} else {
+			this.type = Type.failed;
+		}
+	}
+	
+	public boolean treatFailed() {
+		return type.equals( Type.failed );
+	}
+	
+	public boolean treatSuccessful() {
+		return type.equals( Type.successful );
+	}
+	
+	public Type getType() {
+		return type;
+	}
+	
+	public String toString() {
+		return type.toString();
+	}
+}
