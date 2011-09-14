336b0045c7ba05e7f8cac836b4be176c6cf4e4d6
wolfgarnet <wolfgarnet@gmail.com>
2011-09-14 10:56:05 +0200

    Trying to fix null pointer exception

--- src/main/java/net/praqma/hudson/scm/CCUCMState.java
+++ src/main/java/net/praqma/hudson/scm/CCUCMState.java
@@ -26,356 +26,356 @@ import net.praqma.util.debug.PraqmaLogger.Logger;
  */
 public class CCUCMState {
 
-    private List<State> states = Collections.synchronizedList(new ArrayList<State>());
-    private static final String linesep = System.getProperty("line.separator");
-    private Logger logger = PraqmaLogger.getLogger();
+	private List<State> states = Collections.synchronizedList( new ArrayList<State>() );
+	private static final String linesep = System.getProperty( "line.separator" );
+	private Logger logger = PraqmaLogger.getLogger();
 
-    /**
-     * Get a state given job name and job number
-     * 
-     * @param jobName
-     *            the hudson job name
-     * @param jobNumber
-     *            the hudson job number
-     * @return
-     */
-    public State getState(String jobName, Integer jobNumber) {
-        for (State s : states) {
-            if (s.getJobName().equals(jobName)
-                    && s.getJobNumber().equals(jobNumber)) {
-                return s;
-            }
-        }
+	/**
+	 * Get a state given job name and job number
+	 * 
+	 * @param jobName
+	 *            the hudson job name
+	 * @param jobNumber
+	 *            the hudson job number
+	 * @return
+	 */
+	public State getState( String jobName, Integer jobNumber ) {
+		for( State s : states ) {
+			try {
+				if( s.getJobName().equals( jobName ) && s.getJobNumber().equals( jobNumber ) ) {
+					return s;
+				}
+			} catch( NullPointerException e ) {
+				/* It's ok, let's move on */
+			}
+		}
 
-        State s = new State(jobName, jobNumber);
-        states.add(s);
-        return s;
-    }
+		State s = new State( jobName, jobNumber );
+		states.add( s );
+		return s;
+	}
 
-    public boolean removeState(String jobName, Integer jobNumber) {
-        for (State s : states) {
-            if (s.getJobName().equals(jobName) && s.getJobNumber() == jobNumber) {
-                states.remove(s);
-                return true;
-            }
-        }
+	public boolean removeState( String jobName, Integer jobNumber ) {
+		for( State s : states ) {
+			if( s.getJobName().equals( jobName ) && s.getJobNumber() == jobNumber ) {
+				states.remove( s );
+				return true;
+			}
+		}
 
-        return false;
-    }
+		return false;
+	}
 
-    public State getStateByBaseline(String jobName, String baseline) {
-        for (State s : states) {
-            if (s.getJobName().equals(jobName) && s.getBaseline() != null && s.getBaseline().getFullyQualifiedName().equals(baseline)) {
-                return s;
-            }
-        }
+	public State getStateByBaseline( String jobName, String baseline ) {
+		for( State s : states ) {
+			if( s.getJobName().equals( jobName ) && s.getBaseline() != null && s.getBaseline().getFullyQualifiedName().equals( baseline ) ) {
+				return s;
+			}
+		}
 
-        return null;
-    }
+		return null;
+	}
 
-    public void addState(State state) {
-        this.states.add(state);
-    }
+	public void addState( State state ) {
+		this.states.add( state );
+	}
 
-    public boolean stateExists(State state) {
-        return stateExists(state.jobName, state.jobNumber);
-    }
+	public boolean stateExists( State state ) {
+		return stateExists( state.jobName, state.jobNumber );
+	}
 
-    public boolean stateExists(String jobName, Integer jobNumber) {
-        for (State s : states) {
-            if (s.getJobName().equals(jobName) && s.getJobNumber() == jobNumber) {
-                return true;
-            }
-        }
+	public boolean stateExists( String jobName, Integer jobNumber ) {
+		for( State s : states ) {
+			if( s.getJobName().equals( jobName ) && s.getJobNumber() == jobNumber ) {
+				return true;
+			}
+		}
 
-        return false;
-    }
+		return false;
+	}
 
-    public boolean removeState(State state) {
-        return states.remove(state);
-    }
+	public boolean removeState( State state ) {
+		return states.remove( state );
+	}
 
-    public int recalculate(AbstractProject<?, ?> project) {
-        int count = 0;
+	public int recalculate( AbstractProject<?, ?> project ) {
+		int count = 0;
 
-        try {
-            State s = null;
-            Iterator<State> it = states.iterator();
+		try {
+			State s = null;
+			Iterator<State> it = states.iterator();
 
-            while (it.hasNext()) {
-                s = it.next();
-                Integer bnum = s.getJobNumber();
-                Object o = project.getBuildByNumber(bnum);
-                Build bld = (Build) o;
+			while( it.hasNext() ) {
+				s = it.next();
+				Integer bnum = s.getJobNumber();
+				Object o = project.getBuildByNumber( bnum );
+				Build bld = (Build) o;
 
-                /* The job is not running */
-                if (!bld.isLogUpdated()) {
-                    it.remove();
-                    count++;
-                }
-            }
-        } catch (ConcurrentModificationException e) {
-            logger.warning("Concurrency warning in CCUCMState");
-        } catch (NullPointerException e) {
-            logger.warning("This should not happen");
-        }
+				/* The job is not running */
+				if( !bld.isLogUpdated() ) {
+					it.remove();
+					count++;
+				}
+			}
+		} catch (ConcurrentModificationException e) {
+			logger.warning( "Concurrency warning in CCUCMState" );
+		} catch (NullPointerException e) {
+			logger.warning( "This should not happen" );
+		}
 
-        return count;
-    }
+		return count;
+	}
 
-    public int size() {
-        return states.size();
-    }
+	public int size() {
+		return states.size();
+	}
 
-    public String stringify() {
-        return net.praqma.util.structure.Printer.listPrinterToString(states);
-    }
+	public String stringify() {
+		return net.praqma.util.structure.Printer.listPrinterToString( states );
+	}
 
-    public class State {
+	public class State {
 
-        private Baseline baseline;
-        private Stream stream;
-        private Component component;
-        private boolean doPostBuild = true;
-        private Project.Plevel plevel;
-        private String loadModule;
-        private String jobName;
-        private Integer jobNumber;
-        private boolean addedByPoller = false;
-        private long multiSiteFrequency = 0;
-        private List<Baseline> baselines = null;
-        private Polling polling;
-        private Unstable unstable;
-        private boolean needsToBeCompleted = true;
-        private Logger logger;
-        private SnapshotView snapView;
-        private SnapshotView deliverView;
-        private boolean createBaseline = true;
-        private String nameTemplate;
-        
-        private boolean setDescription = false;
-        private boolean makeTag = false;
-        private boolean recommend = false;
-        
-        private String error;
+		private Baseline baseline;
+		private Stream stream;
+		private Component component;
+		private boolean doPostBuild = true;
+		private Project.Plevel plevel;
+		private String loadModule;
+		private String jobName;
+		private Integer jobNumber;
+		private boolean addedByPoller = false;
+		private long multiSiteFrequency = 0;
+		private List<Baseline> baselines = null;
+		private Polling polling;
+		private Unstable unstable;
+		private boolean needsToBeCompleted = true;
+		private Logger logger;
+		private SnapshotView snapView;
+		private SnapshotView deliverView;
+		private boolean createBaseline = true;
+		private String nameTemplate;
 
-        public State() {
-        }
+		private boolean setDescription = false;
+		private boolean makeTag = false;
+		private boolean recommend = false;
 
-        public State(String jobName, Integer jobNumber) {
-            this.jobName = jobName;
-            this.jobNumber = jobNumber;
-        }
+		private String error;
 
-        public State(String jobName, Integer jobNumber, Baseline baseline,
-                Stream stream, Component component, boolean doPostBuild) {
-            this.jobName = jobName;
-            this.jobNumber = jobNumber;
-            this.baseline = baseline;
-            this.stream = stream;
-            this.component = component;
-            this.doPostBuild = doPostBuild;
-        }
+		public State() {
+		}
 
-        @Deprecated
-        public void save() {
-            CCUCMState.this.addState(this);
-        }
+		public State( String jobName, Integer jobNumber ) {
+			this.jobName = jobName;
+			this.jobNumber = jobNumber;
+		}
 
-        public boolean remove() {
-            return CCUCMState.this.removeState(this);
-        }
+		public State( String jobName, Integer jobNumber, Baseline baseline, Stream stream, Component component, boolean doPostBuild ) {
+			this.jobName = jobName;
+			this.jobNumber = jobNumber;
+			this.baseline = baseline;
+			this.stream = stream;
+			this.component = component;
+			this.doPostBuild = doPostBuild;
+		}
 
-        public Baseline getBaseline() {
-            return baseline;
-        }
+		@Deprecated
+		public void save() {
+			CCUCMState.this.addState( this );
+		}
 
-        public void setBaseline(Baseline baseline) {
-            this.baseline = baseline;
-        }
+		public boolean remove() {
+			return CCUCMState.this.removeState( this );
+		}
 
-        public Stream getStream() {
-            return stream;
-        }
+		public Baseline getBaseline() {
+			return baseline;
+		}
 
-        public Polling getPolling() {
-            return polling;
-        }
+		public void setBaseline( Baseline baseline ) {
+			this.baseline = baseline;
+		}
 
-        public void setPolling(Polling polling) {
-            this.polling = polling;
-        }
-        
-        public Unstable getUnstable() {
-            return unstable;
-        }
+		public Stream getStream() {
+			return stream;
+		}
 
-        public void setUnstable(Unstable unstable) {
-            this.unstable = unstable;
-        }
+		public Polling getPolling() {
+			return polling;
+		}
 
-        public void setStream(Stream stream) {
-            this.stream = stream;
-        }
+		public void setPolling( Polling polling ) {
+			this.polling = polling;
+		}
 
-        public Component getComponent() {
-            return component;
-        }
+		public Unstable getUnstable() {
+			return unstable;
+		}
 
-        public void setComponent(Component component) {
-            this.component = component;
-        }
+		public void setUnstable( Unstable unstable ) {
+			this.unstable = unstable;
+		}
 
-        public boolean doPostBuild() {
-            return doPostBuild;
-        }
+		public void setStream( Stream stream ) {
+			this.stream = stream;
+		}
 
-        public void setPostBuild(boolean doPostBuild) {
-            this.doPostBuild = doPostBuild;
-        }
+		public Component getComponent() {
+			return component;
+		}
 
-        public String getJobName() {
-            return jobName;
-        }
+		public void setComponent( Component component ) {
+			this.component = component;
+		}
 
-        public void setJobName(String jobName) {
-            this.jobName = jobName;
-        }
+		public boolean doPostBuild() {
+			return doPostBuild;
+		}
 
-        public Integer getJobNumber() {
-            return jobNumber;
-        }
+		public void setPostBuild( boolean doPostBuild ) {
+			this.doPostBuild = doPostBuild;
+		}
 
-        public void setJobNumber(Integer jobNumber) {
-            this.jobNumber = jobNumber;
-        }
+		public String getJobName() {
+			return jobName;
+		}
 
-        public void setPlevel(Project.Plevel plevel) {
-            this.plevel = plevel;
-        }
+		public void setJobName( String jobName ) {
+			this.jobName = jobName;
+		}
 
-        public Project.Plevel getPlevel() {
-            return plevel;
-        }
+		public Integer getJobNumber() {
+			return jobNumber;
+		}
 
-        public void setLogger(Logger logger) {
-            this.logger = logger;
-        }
+		public void setJobNumber( Integer jobNumber ) {
+			this.jobNumber = jobNumber;
+		}
 
-        public Logger getLogger() {
-            return logger;
-        }
+		public void setPlevel( Project.Plevel plevel ) {
+			this.plevel = plevel;
+		}
 
-        public String stringify() {
-            StringBuffer sb = new StringBuffer();
+		public Project.Plevel getPlevel() {
+			return plevel;
+		}
 
-            sb.append("Job name      : " + this.jobName + linesep);
-            sb.append("Job number    : " + this.jobNumber + linesep);
-            sb.append("Component     : " + this.component + linesep);
-            sb.append("Stream        : " + this.stream + linesep);
-            sb.append("Baseline      : " + this.baseline + linesep);
-            sb.append("Poll level    : " + (this.plevel != null ? this.plevel.toString() : "Missing") + linesep);
-            sb.append("Load Module   : " + this.loadModule + linesep);
-            sb.append("Baseline list : " + (this.baselines != null ? this.baselines.size() : "0") + linesep);
-            sb.append("Added by poll : " + (this.addedByPoller ? "Yes" : "No") + linesep);
-            sb.append("Multi site    : " + (this.multiSiteFrequency > 0 ? StoredBaselines.milliToMinute(this.multiSiteFrequency) : "N/A") + linesep);
-            sb.append("postBuild     : " + this.doPostBuild + linesep);
+		public void setLogger( Logger logger ) {
+			this.logger = logger;
+		}
 
-            return sb.toString();
-        }
+		public Logger getLogger() {
+			return logger;
+		}
 
-        public String toString() {
-            return "(" + jobName + ", " + jobNumber + ")";
-        }
+		public String stringify() {
+			StringBuffer sb = new StringBuffer();
 
-        public boolean equals(Object other) {
-            if (other instanceof State) {
-                if (this.getJobName().equals(((State) other).getJobName())
-                        && this.getJobNumber().equals(
-                        ((State) other).getJobNumber())) {
-                    return true;
-                }
+			sb.append( "Job name      : " + this.jobName + linesep );
+			sb.append( "Job number    : " + this.jobNumber + linesep );
+			sb.append( "Component     : " + this.component + linesep );
+			sb.append( "Stream        : " + this.stream + linesep );
+			sb.append( "Baseline      : " + this.baseline + linesep );
+			sb.append( "Poll level    : " + ( this.plevel != null ? this.plevel.toString() : "Missing" ) + linesep );
+			sb.append( "Load Module   : " + this.loadModule + linesep );
+			sb.append( "Baseline list : " + ( this.baselines != null ? this.baselines.size() : "0" ) + linesep );
+			sb.append( "Added by poll : " + ( this.addedByPoller ? "Yes" : "No" ) + linesep );
+			sb.append( "Multi site    : " + ( this.multiSiteFrequency > 0 ? StoredBaselines.milliToMinute( this.multiSiteFrequency ) : "N/A" ) + linesep );
+			sb.append( "postBuild     : " + this.doPostBuild + linesep );
 
-            }
+			return sb.toString();
+		}
 
-            return false;
-        }
+		public String toString() {
+			return "(" + jobName + ", " + jobNumber + ")";
+		}
 
-        public void setLoadModule(String loadModule) {
-            this.loadModule = loadModule;
-        }
+		public boolean equals( Object other ) {
+			if( other instanceof State ) {
+				if( this.getJobName().equals( ( (State) other ).getJobName() ) && this.getJobNumber().equals( ( (State) other ).getJobNumber() ) ) {
+					return true;
+				}
 
-        public String getLoadModule() {
-            return loadModule;
-        }
+			}
 
-        public void setAddedByPoller(boolean addedByPoller) {
-            this.addedByPoller = addedByPoller;
-        }
+			return false;
+		}
 
-        public boolean isAddedByPoller() {
-            return addedByPoller;
-        }
+		public void setLoadModule( String loadModule ) {
+			this.loadModule = loadModule;
+		}
 
-        public void setMultiSiteFrequency(long multiSiteFrquency) {
-            this.multiSiteFrequency = multiSiteFrquency;
-        }
+		public String getLoadModule() {
+			return loadModule;
+		}
 
-        public long getMultiSiteFrquency() {
-            return multiSiteFrequency;
-        }
+		public void setAddedByPoller( boolean addedByPoller ) {
+			this.addedByPoller = addedByPoller;
+		}
 
-        public boolean isMultiSite() {
-            return this.multiSiteFrequency > 0;
-        }
+		public boolean isAddedByPoller() {
+			return addedByPoller;
+		}
 
-        public void setBaselines(List<Baseline> baselines) {
-            this.baselines = baselines;
-        }
+		public void setMultiSiteFrequency( long multiSiteFrquency ) {
+			this.multiSiteFrequency = multiSiteFrquency;
+		}
 
-        public List<Baseline> getBaselines() {
-            return baselines;
-        }
+		public long getMultiSiteFrquency() {
+			return multiSiteFrequency;
+		}
 
-        public SnapshotView getSnapView() {
-            return this.snapView;
-        }
-        
-        public void setSnapView(SnapshotView snapView) {
-            this.snapView = snapView;
-        }
-        
-        public SnapshotView getDeliverView() {
-            return this.deliverView;
-        }
-        
-        public void setDeliverView(SnapshotView deliverView) {
-            this.deliverView = deliverView;
-        }
-        
-        public void setNeedsToBeCompleted( boolean s ) {
-            this.needsToBeCompleted = s;
-        }
-        
-        public boolean needsToBeCompleted() {
-            return this.needsToBeCompleted;
-        }
-        
-        public void setCreatebaseline( boolean tf ) {
-        	this.createBaseline = tf;
-        }
-        
-        public boolean createBaseline() {
-        	return this.createBaseline;
-        }
-        
-        public void setNameTemplate( String template ) {
-        	this.nameTemplate = template;
-        }
-        
-        public String getNameTemplate() {
-        	return this.nameTemplate;
-        }
+		public boolean isMultiSite() {
+			return this.multiSiteFrequency > 0;
+		}
+
+		public void setBaselines( List<Baseline> baselines ) {
+			this.baselines = baselines;
+		}
+
+		public List<Baseline> getBaselines() {
+			return baselines;
+		}
+
+		public SnapshotView getSnapView() {
+			return this.snapView;
+		}
+
+		public void setSnapView( SnapshotView snapView ) {
+			this.snapView = snapView;
+		}
+
+		public SnapshotView getDeliverView() {
+			return this.deliverView;
+		}
+
+		public void setDeliverView( SnapshotView deliverView ) {
+			this.deliverView = deliverView;
+		}
+
+		public void setNeedsToBeCompleted( boolean s ) {
+			this.needsToBeCompleted = s;
+		}
+
+		public boolean needsToBeCompleted() {
+			return this.needsToBeCompleted;
+		}
+
+		public void setCreatebaseline( boolean tf ) {
+			this.createBaseline = tf;
+		}
+
+		public boolean createBaseline() {
+			return this.createBaseline;
+		}
+
+		public void setNameTemplate( String template ) {
+			this.nameTemplate = template;
+		}
+
+		public String getNameTemplate() {
+			return this.nameTemplate;
+		}
 
 		public String getError() {
 			return error;
@@ -408,7 +408,6 @@ public class CCUCMState {
 		public void setRecommend( boolean recommend ) {
 			this.recommend = recommend;
 		}
-        
-        
-    }
+
+	}
 }
