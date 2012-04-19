56bd994a4ec1dae4af53704eefe586aad89c4361
wolfgarnet <wolfgarnet@gmail.com>
2012-04-19 18:13:57 +0200

    Merged load entity

--- src/main/java/net/praqma/hudson/remoting/LoadEntity.java
+++ src/main/java/net/praqma/hudson/remoting/LoadEntity.java
@@ -5,7 +5,7 @@ import java.io.IOException;
 import java.io.PrintStream;
 import java.util.Set;
 
-import net.praqma.clearcase.ucm.UCMException;
+import net.praqma.clearcase.exceptions.ClearCaseException;
 import net.praqma.clearcase.ucm.entities.UCM;
 import net.praqma.clearcase.ucm.entities.UCMEntity;
 import net.praqma.util.debug.Logger;
@@ -23,47 +23,46 @@ public class LoadEntity implements FileCallable<UCMEntity> {
 
 	private UCMEntity entity;
 	private Pipe pipe;
-	
+
 	private LoggerSetting loggerSetting;
 	private PrintStream pstream;
-	
+
 	public LoadEntity( UCMEntity entity, Pipe pipe, PrintStream pstream, LoggerSetting loggerSetting ) {
 		this.entity = entity;
 		this.pipe = pipe;
-		
+
 		this.pstream = pstream;
-		
+
 		this.loggerSetting = loggerSetting;
-    }
-    
-    @Override
-    public UCMEntity invoke( File f, VirtualChannel channel ) throws IOException, InterruptedException {
-        
-    	StreamAppender app = null;
-    	if( pipe != null ) {
-	    	PrintStream toMaster = new PrintStream( pipe.getOut() );
-	    	app = new StreamAppender( toMaster );
-	    	app.lockToCurrentThread();
-	    	Logger.addAppender( app );
-	    	app.setSettings( loggerSetting );
-    	} else if( pstream != null ) {
-	    	app = new StreamAppender( pstream );
-	    	app.lockToCurrentThread();
-	    	Logger.addAppender( app );
-	    	app.setSettings( loggerSetting );    		
-    	}
-        
-    	try {
-    		UCM.setContext( UCM.ContextType.CLEARTOOL );
-			entity.load();
-		} catch (UCMException e) {
-        	Logger.removeAppender( app );
-        	throw new IOException( "Unable to load " + entity.getShortname() + ":" + e.getMessage() );
+	}
+
+	@Override
+	public UCMEntity invoke( File f, VirtualChannel channel ) throws IOException, InterruptedException {
+
+		StreamAppender app = null;
+		if( pipe != null ) {
+			PrintStream toMaster = new PrintStream( pipe.getOut() );
+			app = new StreamAppender( toMaster );
+			app.lockToCurrentThread();
+			Logger.addAppender( app );
+			app.setSettings( loggerSetting );
+		} else if( pstream != null ) {
+			app = new StreamAppender( pstream );
+			app.lockToCurrentThread();
+			Logger.addAppender( app );
+			app.setSettings( loggerSetting );
 		}
 
-    	Logger.removeAppender( app );
+		try {
+			entity.load();
+		} catch( ClearCaseException e ) {
+			Logger.removeAppender( app );
+			throw new IOException( "Unable to load " + entity.getShortname() + ":" + e.getMessage() );
+		}
 
-    	return entity;
-    }
+		Logger.removeAppender( app );
+
+		return entity;
+	}
 
 }
