5b0b4597c8e592ffca9818ff193ea91019b03ec6
wolfgarnet <wolfgarnet@gmail.com>
2012-04-19 18:12:43 +0200

    Merged end view

--- src/main/java/net/praqma/hudson/remoting/EndView.java
+++ src/main/java/net/praqma/hudson/remoting/EndView.java
@@ -3,7 +3,7 @@ package net.praqma.hudson.remoting;
 import java.io.File;
 import java.io.IOException;
 
-import net.praqma.clearcase.ucm.UCMException;
+import net.praqma.clearcase.exceptions.ClearCaseException;
 import net.praqma.clearcase.ucm.view.SnapshotView;
 
 import hudson.FilePath.FileCallable;
@@ -14,21 +14,21 @@ public class EndView implements FileCallable<Boolean> {
 	private static final long serialVersionUID = -8984877325832486334L;
 
 	private String viewtag;
-	
+
 	public EndView( String viewtag ) {
 		this.viewtag = viewtag;
-    }
-    
-    @Override
-    public Boolean invoke( File f, VirtualChannel channel ) throws IOException, InterruptedException {
+	}
 
-    	try {
-    		SnapshotView.endView( viewtag );
-		} catch (UCMException e) {
-        	throw new IOException( "Unable to end the view " + viewtag + ": " + e.getMessage() );
+	@Override
+	public Boolean invoke( File f, VirtualChannel channel ) throws IOException, InterruptedException {
+
+		try {
+			SnapshotView.end( viewtag );
+		} catch( ClearCaseException e ) {
+			throw new IOException( "Unable to end the view " + viewtag + ": " + e.getMessage() );
 		}
 
-    	return true;
-    }
+		return true;
+	}
 
 }
