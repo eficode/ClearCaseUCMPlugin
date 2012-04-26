ed2d145fa1fa2cde0b5498d920ac4fa2bb71632c
wolfgarnet <wolfgarnet@gmail.com>
2012-04-26 13:29:09 +0200

    Fixed

--- src/test/java/net/praqma/hudson/scm/ScmTest.java
+++ src/test/java/net/praqma/hudson/scm/ScmTest.java
@@ -28,7 +28,7 @@ public class ScmTest extends ClearCaseJenkinsTestCase {
 		coolTest.bootStrap();
 		FreeStyleProject project = createFreeStyleProject( "ccucm" );
 		
-		CCUCMScm scm = new CCUCMScm( "Model@" + coolTest.getPVob(), "INITIAL", "ALL", false, "self", "cc1_one_int@" + coolTest.getPVob(), "successful", false, "", true, true, false, true, "jenkins" );
+		CCUCMScm scm = new CCUCMScm( "Model@" + coolTest.getPVob(), "INITIAL", "ALL", false, "self", "cc1_one_dev@" + coolTest.getPVob(), "successful", false, "", true, true, false, true, "jenkins" );
 		
 		project.setScm( scm );
 		
