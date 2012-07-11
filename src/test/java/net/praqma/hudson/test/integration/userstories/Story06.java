941ec1358bb542d2530e0de5719e823f34366f44
wolfgarnet <wolfgarnet@gmail.com>
2012-07-11 08:39:57 +0200

    Corrected story 6 naming

--- src/test/java/net/praqma/hudson/test/integration/userstories/Story06.java
+++ src/test/java/net/praqma/hudson/test/integration/userstories/Story06.java
@@ -37,7 +37,7 @@ public class Story06 {
 	public static CCUCMRule jenkins = new CCUCMRule();
 	
 	@Rule
-	public static ClearCaseRule ccenv = new ClearCaseRule( "ccucm-story05", "setup-story5.xml" );
+	public static ClearCaseRule ccenv = new ClearCaseRule( "ccucm-story06", "setup-story5.xml" );
 	
 	@Rule
 	public static DescriptionRule desc = new DescriptionRule();
