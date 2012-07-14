72f6f359b0e59f642b284d448459fed9dedff8d1
wolfgarnet <wolfgarnet@gmail.com>
2012-07-14 23:27:38 +0200

    Added story 11 to test tags not installed

---
+++ src/test/java/net/praqma/hudson/test/integration/userstories/Story11.java
@@ -0,0 +1,40 @@
+package net.praqma.hudson.test.integration.userstories;
+
+import org.junit.ClassRule;
+import org.junit.Rule;
+import org.junit.Test;
+
+import hudson.model.AbstractBuild;
+import hudson.model.Result;
+import net.praqma.clearcase.ucm.entities.Baseline;
+import net.praqma.hudson.test.CCUCMRule;
+import net.praqma.hudson.test.SystemValidator;
+import net.praqma.junit.DescriptionRule;
+import net.praqma.junit.TestDescription;
+import net.praqma.util.debug.Logger;
+
+import net.praqma.clearcase.test.junit.ClearCaseRule;
+
+public class Story11 {
+
+	@ClassRule
+	public static CCUCMRule jenkins = new CCUCMRule();
+	
+	@Rule
+	public static ClearCaseRule ccenv = new ClearCaseRule( "ccucm-story11" );
+	
+	@Rule
+	public static DescriptionRule desc = new DescriptionRule();
+
+	@Test
+	@TestDescription( title = "Story 11", text = "A baseline ís built successfully, but the tagging is not done, because the tag tyoe is not installed", configurations = { "Tag = true" }	)
+	public void story11() throws Exception {
+		
+		/* First build to create a view */
+		AbstractBuild<?, ?> build = jenkins.initiateBuild( ccenv.getUniqueName(), "self", "_System@" + ccenv.getPVob(), "one_int@" + ccenv.getPVob(), false, true, false, false, false, false );
+		Baseline baseline = ccenv.context.baselines.get( "model-1" );
+		new SystemValidator( build ).validateBuild( Result.FAILURE ).validateBaselineTag( baseline, false ).validate();
+	}
+
+	
+}
