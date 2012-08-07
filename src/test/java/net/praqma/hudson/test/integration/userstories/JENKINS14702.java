3dc202ac6070da9d5336c525605e86739ea78bc7
wolfgarnet <wolfgarnet@gmail.com>
2012-08-07 10:33:37 +0200

    [JENKINS-14702] Updating test

--- src/test/java/net/praqma/hudson/test/integration/userstories/JENKINS14702.java
+++ src/test/java/net/praqma/hudson/test/integration/userstories/JENKINS14702.java
@@ -6,6 +6,8 @@ import org.junit.Test;
 
 import hudson.model.AbstractBuild;
 import hudson.model.Result;
+import net.praqma.clearcase.ucm.entities.Baseline;
+import net.praqma.clearcase.ucm.entities.Project.PromotionLevel;
 import net.praqma.hudson.test.CCUCMRule;
 import net.praqma.hudson.test.SystemValidator;
 import net.praqma.junit.DescriptionRule;
@@ -28,8 +30,10 @@ public class JENKINS14702 {
 	@TestDescription( title = "JENKINS-14702", text = "Use the current streams project, if the jenkins build project is not found", configurations = { "Jenkins project = not created" }	)
 	public void jenkins13944() throws Exception {
 	
+		/* First build to create a view */
 		AbstractBuild<?, ?> build = jenkins.initiateBuild( ccenv.getUniqueName(), "self", "_System@" + ccenv.getPVob(), "one_int@" + ccenv.getPVob(), false, false, false, false, false, false );
-		new SystemValidator( build ).validateBuild( Result.FAILURE ).validateBuiltBaselineNotFound().validate();
+		Baseline baseline = ccenv.context.baselines.get( "model-1" );
+		new SystemValidator( build ).validateBuild( Result.SUCCESS ).validateBuiltBaseline( PromotionLevel.BUILT, baseline, false ).validate();
 	}
 
 	
