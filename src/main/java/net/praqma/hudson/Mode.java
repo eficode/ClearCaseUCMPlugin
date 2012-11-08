869cfa340645f12729865f2c3890fbfbba549c2e
Christian Wolfgang <wolfgarnet@gmail.com>
2012-11-08 21:44:58 +0100

    Added mode

---
+++ src/main/java/net/praqma/hudson/Mode.java
@@ -0,0 +1,18 @@
+package net.praqma.hudson;
+
+/**
+ * User: cwolfgang
+ * Date: 08-11-12
+ * Time: 21:23
+ */
+public class Mode {
+
+    private CCUCMBuildAction action;
+
+
+    public abstract
+
+    public static Mode getMode( CCUCMBuildAction action ) {
+
+    }
+}
