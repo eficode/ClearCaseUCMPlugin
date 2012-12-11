f88d969be4ac6ab6f750b758decb35059f50a898
Mads Nielsen <inkspot84@gmail.com>
2012-12-11 14:11:26 +0100

    Added comments for test JENKINS-16072

--- src/test/java/net/praqma/hudson/test/integration/userstories/JENKINS16072.java
+++ src/test/java/net/praqma/hudson/test/integration/userstories/JENKINS16072.java
@@ -6,7 +6,6 @@ package net.praqma.hudson.test.integration.userstories;
 
 import java.io.File;
 import java.io.FileOutputStream;
-import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import net.praqma.clearcase.ucm.utils.BaselineList;
 import net.praqma.clearcase.ucm.utils.filters.NoDeliver;
@@ -19,6 +18,12 @@ import org.junit.Test;
  */
 public class JENKINS16072 extends BaseTestClass {
     
+    /**
+     * This is a small test to ensure that the BaseLine list object is serialized proberly. One thing to note is that when you design classes which
+     * have associations or belong to another class, remember to ensure that ALL member classes are serializable if the object is used 
+     * with Jenkins remoting. (Building on a remote slave).
+     * @throws Exception 
+     */
     @Test
     public void jenkins_16072() throws Exception {
         File testFile = File.createTempFile("objectSerialization", ".test");
