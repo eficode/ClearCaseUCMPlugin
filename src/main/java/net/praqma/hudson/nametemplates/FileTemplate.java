62ca813885dc6c89387c2bf1a2d3d8b805bb77ea
Mads Nielsen <inkspot84@gmail.com>
2013-01-29 14:58:00 +0100

    Attempt at fixing issues with baseline file template

--- src/main/java/net/praqma/hudson/nametemplates/FileTemplate.java
+++ src/main/java/net/praqma/hudson/nametemplates/FileTemplate.java
@@ -7,19 +7,18 @@ import net.praqma.hudson.CCUCMBuildAction;
 
 public class FileTemplate extends Template {
 	
-	private Logger logger = Logger.getLogger( FileTemplate.class.getName() );
+	private static final Logger logger = Logger.getLogger( FileTemplate.class.getName() );
 
 	@Override
 	public String parse( CCUCMBuildAction action, String filename ) {
 		try {
-			FilePath file = new FilePath(new File(action.getWorkspace() + filename) );
-			logger.fine( "FILE: " + file );
+			FilePath file = new FilePath(new File(action.getWorkspace() + System.getProperty("file.separator") + filename) );            
+            logger.fine( String.format( "Full path to the remote %s", file.absolutize() ) );
+			logger.fine( String.format( "FILE: %s", file ) );
 			return file.readToString().trim();
-		} catch( Exception e ) {
-			
-			logger.fine( "E: " + e.getMessage() );
+		} catch( Exception e ) {			
+			logger.fine( "E: " + e.getMessage() );            
 			return "?";
 		}
 	}
-
 }
