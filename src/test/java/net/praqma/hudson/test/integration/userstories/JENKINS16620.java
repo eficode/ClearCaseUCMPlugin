9c9f55c1ff399eb1d24b2b55f4b5e10bdd148405
Christian Wolfgang <wolfgarnet@gmail.com>
2013-02-05 10:47:16 +0100

    [JENKINS-16620] Any should not be able to get changes after resetting a baseline

--- src/test/java/net/praqma/hudson/test/integration/userstories/JENKINS16620.java
+++ src/test/java/net/praqma/hudson/test/integration/userstories/JENKINS16620.java
@@ -14,6 +14,7 @@ import net.praqma.junit.TestDescription;
 import org.junit.Rule;
 import org.junit.Test;
 
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 /**
@@ -62,7 +63,7 @@ public class JENKINS16620 extends BaseTestClass {
 
         PollingResult result = project.poll( jenkins.createTaskListener() );
 
-        assertTrue( result.hasChanges() );
+        assertFalse( result.hasChanges() );
     }
 
 }
