06d0f698bf562f1bfb821f5435562847a905f9c4
Christian Wolfgang <wolfgarnet@gmail.com>
2013-02-05 21:27:59 +0100

    [JENKINS-16487] Added test

---
+++ src/test/java/net/praqma/hudson/test/integration/userstories/JENKINS16487.java
@@ -0,0 +1,47 @@
+package net.praqma.hudson.test.integration.userstories;
+
+import hudson.model.AbstractBuild;
+import hudson.model.Project;
+import hudson.model.Result;
+import net.praqma.clearcase.test.annotations.ClearCaseUniqueVobName;
+import net.praqma.clearcase.test.junit.ClearCaseRule;
+import net.praqma.hudson.test.BaseTestClass;
+import net.praqma.hudson.test.CCUCMRule;
+import net.praqma.hudson.test.SystemValidator;
+import net.praqma.junit.DescriptionRule;
+import net.praqma.junit.TestDescription;
+import org.junit.Rule;
+import org.junit.Test;
+
+/**
+ * @author cwolfgang
+ *         Date: 04-02-13
+ *         Time: 12:00
+ */
+public class JENKINS16487 extends BaseTestClass {
+
+    @Rule
+    public static ClearCaseRule ccenv = new ClearCaseRule( "JENKINS-16487", "setup-JENKINS-16620.xml" );
+
+    @Rule
+    public static DescriptionRule desc = new DescriptionRule();
+
+    @Test
+    @TestDescription( title = "JENKINS-16487", text = "Promotion Level being set to INITIAL" )
+    @ClearCaseUniqueVobName( name = "ANY" )
+    public void jenkins16487Any() throws Exception {
+        Project project = new CCUCMRule.ProjectCreator( "JENKINS-16487-any", "_System@" + ccenv.getPVob(), "one_int@" + ccenv.getPVob() ).setPromotionLevel( null ).getProject();
+
+        /* First build must be a success, because there is a valid baseline.
+         * This build is done because we need a previous action object */
+        AbstractBuild build1 = new CCUCMRule.ProjectBuilder( project ).build();
+        new SystemValidator( build1 ).validateBuild( Result.SUCCESS ).validate();
+
+        /* Because we have ANY promotion level, the build must NOT fail */
+        AbstractBuild build2 = new CCUCMRule.ProjectBuilder( project ).build();
+        new SystemValidator( build2 ).validateBuild( Result.SUCCESS ).validate();
+    }
+
+
+
+}
