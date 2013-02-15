8a8ec6af785ec83d91f82c885e20dd63b19b2157
Christian Wolfgang <wolfgarnet@gmail.com>
2013-02-15 08:57:27 +0100

    Added logger listener

---
+++ src/main/java/net/praqma/hudson/LoggerListener.java
@@ -0,0 +1,28 @@
+package net.praqma.hudson;
+
+import hudson.Extension;
+import hudson.model.Run;
+import hudson.model.TaskListener;
+import hudson.model.listeners.RunListener;
+import net.praqma.logging.LoggingUtil;
+
+import java.util.logging.Level;
+
+/**
+ * @author cwolfgang
+ *         Date: 15-02-13
+ *         Time: 08:57
+ */
+@Extension
+public class LoggerListener extends RunListener<Run> {
+
+    public LoggerListener() {
+        super( Run.class );
+        System.out.println( "Initializing logger for test" );
+    }
+
+    @Override
+    public void onStarted( Run run, TaskListener listener ) {
+        LoggingUtil.setPraqmaticHandler( Level.ALL, "net.praqma" );
+    }
+}
