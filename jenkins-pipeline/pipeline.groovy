684746f00aef23628c58e1d9c9b7a87d714352d7
Mads Nielsen <man@praqma.net>
2018-07-12 12:03:40 +0200

    Add correct traits to job dsl

--- jenkins-pipeline/pipeline.groovy
+++ jenkins-pipeline/pipeline.groovy
@@ -16,7 +16,8 @@ multibranchPipelineJob("ClearCase UCM Jenkins Plugin") {
     }
 
     configure {
-        it / 'sources' / 'data' / 'jenkins.branch.BranchSource' / 'source' / 'traits' << 'jenkins.plugins.git.traits.CloneOptionTrait' {
+        def traitBlock = it / 'sources' / 'data' / 'jenkins.branch.BranchSource' / 'source' / 'traits'
+        traitBlock << 'jenkins.plugins.git.traits.CloneOptionTrait' {
             extension(class: 'hudson.plugins.git.extensions.impl.CloneOption') {
                 shallow(false)
                 noTag(false)
@@ -25,5 +26,8 @@ multibranchPipelineJob("ClearCase UCM Jenkins Plugin") {
                 honorRefspec(false)
             }
         }
+
+        traitBlock << 'jenkins.plugins.git.traits.BranchDiscoveryTrait' { }
     }
+
 }
