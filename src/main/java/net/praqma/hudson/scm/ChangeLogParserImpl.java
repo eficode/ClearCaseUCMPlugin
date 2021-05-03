02163a46dce4247c9505ee4b951004f43aed0cb3
Ramon Leon <manuelramonleonjimenez@gmail.com>
2021-05-03 13:47:25 +0200

    [JENKINS-65161] Remove usage of Digester taken from Jenkins Core

--- src/main/java/net/praqma/hudson/scm/ChangeLogParserImpl.java
+++ src/main/java/net/praqma/hudson/scm/ChangeLogParserImpl.java
@@ -8,14 +8,15 @@ import java.util.List;
 import java.util.logging.Logger;
 
 import edu.umd.cs.findbugs.annotations.*;
-import org.apache.commons.digester.Digester;
+import org.apache.commons.digester3.Digester;
 import org.xml.sax.SAXException;
 
 import hudson.model.AbstractBuild;
 import hudson.scm.ChangeLogParser;
 import hudson.scm.ChangeLogSet;
 import hudson.scm.ChangeLogSet.Entry;
-import hudson.util.Digester2;
+
+import javax.xml.parsers.ParserConfigurationException;
 import java.util.logging.Level;
 
 /**
@@ -29,10 +30,26 @@ public class ChangeLogParserImpl extends ChangeLogParser {
 
 	protected static final Logger logger = Logger.getLogger( ChangeLogParserImpl.class.getName() );
 
+	private Digester createDigester(boolean secure) throws SAXException {
+		Digester digester = new Digester();
+		if (secure) {
+			digester.setXIncludeAware(false);
+			try {
+				digester.setFeature("http://apache.org/xml/features/disallow-doctype-decl", true);
+				digester.setFeature("http://xml.org/sax/features/external-general-entities", false);
+				digester.setFeature("http://xml.org/sax/features/external-parameter-entities", false);
+				digester.setFeature("http://apache.org/xml/features/nonvalidating/load-external-dtd", false);
+			} catch (ParserConfigurationException ex) {
+				throw new SAXException("Failed to securely configure xml digester parser", ex);
+			}
+		}
+		return digester;
+	}
+	
 	@Override
 	public ChangeLogSet<? extends Entry> parse( AbstractBuild build, File changelogFile ) throws IOException, SAXException {
 		List<ChangeLogEntryImpl> entries = new ArrayList<ChangeLogEntryImpl>();
-		Digester digester = new Digester2();
+		Digester digester = createDigester(true);
 		digester.push( entries );
 		digester.addObjectCreate( "*/entry/activity", ChangeLogEntryImpl.class );
 		digester.addSetProperties( "*/entry/activity" );
